867ea9e6d501ded9e799b30db1776c9dde8aa540
frodillo <felipe@cmptc.com>
2015-10-12 09:22:23 -0300

    escena titulos ahora funciona

--- Carreras/Assets/Scripts/EscenaTitulos.cs
+++ Carreras/Assets/Scripts/EscenaTitulos.cs
@@ -3,13 +3,7 @@ using System.Collections;
 
 public class EscenaTitulos : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void Continuar() {
+		Application.LoadLevel( "Escena1" );
 	}
 }
